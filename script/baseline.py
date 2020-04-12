15c66bf51d96fe46560219eb00fccc3d5e85f047
kyohei <h27.kubb@gmail.com>
2020-04-12 16:19:57 +0900

    交差検証の方法を後半28日で行うように修正

--- script/baseline.py
+++ script/baseline.py
@@ -16,6 +16,100 @@ from tqdm import tqdm
 from scipy.sparse import csr_matrix
 
 warnings.filterwarnings('ignore')
+"""
+TODO validデータのおいて現在の予測と正解が最も乖離しているアイテムは何かをEDA
+"""
+
+class WRMSSEEvaluator(object):
+
+    def __init__(self, train_df: pd.DataFrame, valid_df: pd.DataFrame, calendar: pd.DataFrame, prices: pd.DataFrame):
+        train_y = train_df.loc[:, train_df.columns.str.startswith('d_')]
+        train_target_columns = train_y.columns.tolist()
+        weight_columns = train_y.iloc[:, -28:].columns.tolist()
+
+        train_df['all_id'] = 0  # for lv1 aggregation
+
+        id_columns = train_df.loc[:, ~train_df.columns.str.startswith('d_')].columns.tolist()
+        valid_target_columns = valid_df.loc[:, valid_df.columns.str.startswith('d_')].columns.tolist()
+
+        if not all([c in valid_df.columns for c in id_columns]):
+            valid_df = pd.concat([train_df[id_columns], valid_df], axis=1, sort=False)
+
+        self.train_df = train_df
+        self.valid_df = valid_df
+        self.calendar = calendar
+        self.prices = prices
+
+        self.weight_columns = weight_columns
+        self.id_columns = id_columns
+        self.valid_target_columns = valid_target_columns
+
+        weight_df = self.get_weight_df()
+
+        self.group_ids = (
+            'all_id',
+            'state_id',
+            'store_id',
+            'cat_id',
+            'dept_id',
+            ['state_id', 'cat_id'],
+            ['state_id', 'dept_id'],
+            ['store_id', 'cat_id'],
+            ['store_id', 'dept_id'],
+            'item_id',
+            ['item_id', 'state_id'],
+            ['item_id', 'store_id']
+        )
+
+        for i, group_id in enumerate(tqdm(self.group_ids)):
+            train_y = train_df.groupby(group_id)[train_target_columns].sum()
+            scale = []
+            for _, row in train_y.iterrows():
+                series = row.values[np.argmax(row.values != 0):]
+                scale.append(((series[1:] - series[:-1]) ** 2).mean())
+            setattr(self, f'lv{i + 1}_scale', np.array(scale))
+            setattr(self, f'lv{i + 1}_train_df', train_y)
+            setattr(self, f'lv{i + 1}_valid_df', valid_df.groupby(group_id)[valid_target_columns].sum())
+
+            lv_weight = weight_df.groupby(group_id)[weight_columns].sum().sum(axis=1)
+            setattr(self, f'lv{i + 1}_weight', lv_weight / lv_weight.sum())
+
+    def get_weight_df(self) -> pd.DataFrame:
+        day_to_week = self.calendar.set_index('d')['wm_yr_wk'].to_dict()
+        weight_df = self.train_df[['item_id', 'store_id'] + self.weight_columns].set_index(['item_id', 'store_id'])
+        weight_df = weight_df.stack().reset_index().rename(columns={'level_2': 'd', 0: 'value'})
+        weight_df['wm_yr_wk'] = weight_df['d'].map(day_to_week)
+
+        weight_df = weight_df.merge(self.prices, how='left', on=['item_id', 'store_id', 'wm_yr_wk'])
+        weight_df['value'] = weight_df['value'] * weight_df['sell_price']
+        weight_df = weight_df.set_index(['item_id', 'store_id', 'd']).unstack(level=2)['value']
+        weight_df = weight_df.loc[zip(self.train_df.item_id, self.train_df.store_id), :].reset_index(drop=True)
+        weight_df = pd.concat([self.train_df[self.id_columns], weight_df], axis=1, sort=False)
+        return weight_df
+
+    def rmsse(self, valid_preds: pd.DataFrame, lv: int) -> pd.Series:
+        valid_y = getattr(self, f'lv{lv}_valid_df')
+        score = ((valid_y - valid_preds) ** 2).mean(axis=1)
+        scale = getattr(self, f'lv{lv}_scale')
+        return (score / scale).map(np.sqrt)
+
+    def score(self, valid_preds: Union[pd.DataFrame, np.ndarray]) -> float:
+        assert self.valid_df[self.valid_target_columns].shape == valid_preds.shape
+
+        if isinstance(valid_preds, np.ndarray):
+            valid_preds = pd.DataFrame(valid_preds, columns=self.valid_target_columns)
+
+        valid_preds = pd.concat([self.valid_df[self.id_columns], valid_preds], axis=1, sort=False)
+
+        all_scores = []
+        for i, group_id in enumerate(self.group_ids):
+            lv_scores = self.rmsse(valid_preds.groupby(group_id)[self.valid_target_columns].sum(), i + 1)
+            weight = getattr(self, f'lv{i + 1}_weight')
+            lv_scores = pd.concat([weight, lv_scores], axis=1, sort=False).prod(axis=1)
+            all_scores.append(lv_scores.sum())
+
+        return np.mean(all_scores)
+
 
 # function to read the data and merge it
 # TODO 中身を一度確認しつつコードをより分かりやすく整理する
@@ -62,7 +156,9 @@ def create_data(is_train, num_train_day):
     data_df = data_df.merge(sell_prices_df, on = ["store_id", "item_id", "wm_yr_wk"], copy = False)
     all_day = data_df["d"].unique()
     print("data: {0} ~ {1} -> {2}".format(all_day[0], all_day[-1], len(all_day)))
-    
+    del train_df, calendar_df, sell_prices_df
+    gc.collect()
+
     return data_df
 
 
@@ -106,26 +202,47 @@ def feature_engineering(data_df):
 
 # 交差検証はランダムサンプリングで疑似的に行う
 # TODO validationの工夫
-def train_val_split(train_df, features):
+# def train_val_split(train_df, features):
+
+#     # train data
+#     X_train = train_df[features]
+#     y_train = train_df['demand']
+
+#     # valid data
+#     # This is just a subsample of the training set, not a real validation set !
+#     print("\n[CHECK] This valid data is not a real valid data, just random sampling data from train data!")
+
+#     fake_valid_inds = np.random.choice(len(X_train), 2000000, replace=False)
+#     train_inds = np.setdiff1d(X_train.index.values, fake_valid_inds)
+
+#     X_train = X_train.loc[train_inds]
+#     X_train = y_train.loc[train_inds]
+#     X_val = X_train.iloc[fake_valid_inds]
+#     y_val = y_train.iloc[fake_valid_inds]
+
+#     print("train:{}".format(len(X_train)/(len(X_train)+len(X_val))))
+#     print("valid:{}".format(len(X_val)/(len(X_train)+len(X_val))))
+
+#     del train_df
+#     gc.collect()
+
+#     return X_train, y_train, X_val, y_val
+
+
+def train_val_split(train_df):
+    print("train/valid split ->")
 
     # train data
-    X_train = train_df[features]
-    y_train = train_df['demand']
+    train_day = train_df["d"].unique()[:-28]
+    X_train = train_df[train_df["d"].isin(train_day)]
+    y_train = X_train['demand']
+    print("train(day): {0} ~ {1} -> {2}".format(train_day[0], train_day[-1], len(train_day)))
 
     # valid data
-    # This is just a subsample of the training set, not a real validation set !
-    print("\n[CHECK] This valid data is not a real valid data, just random sampling data from train data!")
-
-    fake_valid_inds = np.random.choice(len(X_train), 2000000, replace=False)
-    train_inds = np.setdiff1d(X_train.index.values, fake_valid_inds)
-
-    X_train = X_train.loc[train_inds]
-    X_train = y_train.loc[train_inds]
-    X_val = X_train.iloc[fake_valid_inds]
-    y_val = y_train.iloc[fake_valid_inds]
-
-    print("train:{}".format(len(X_train)/(len(X_train)+len(X_val))))
-    print("valid:{}".format(len(X_val)/(len(X_train)+len(X_val))))
+    valid_day = train_df["d"].unique()[-28:]
+    X_val = train_df[train_df["d"].isin(valid_day)]
+    y_val = X_val['demand']
+    print("valid(day): {0} ~ {1} -> {2}".format(valid_day[0], valid_day[-1], len(valid_day)))
 
     del train_df
     gc.collect()
@@ -133,9 +250,22 @@ def train_val_split(train_df, features):
     return X_train, y_train, X_val, y_val
 
 
+def evaluate_WRMSSE():
+    train_df = pd.read_csv('../input/m5-forecasting-accuracy/sales_train_validation.csv')
+    calendar_df = pd.read_csv("../input/m5-forecasting-accuracy/calendar.csv")
+    sell_prices_df = pd.read_csv("../input/m5-forecasting-accuracy/sell_prices.csv")
+    train_df.sort_values("id", inplace=True).reset_index(drop=True)
+    train_fold_df = train_df.iloc[:, :-28]
+    valid_fold_df = train_df.iloc[:, -28:]
+    del train_df
+    gc.collect()
+
+    val_pred = valid_fold_df.copy() + np.random.randint(100, size=valid_fold_df.shape)
+    evaluator = WRMSSEEvaluator(train_fold_df, valid_fold_df, calendar_df, sell_prices_df)
+    return evaluator.score(val_pred)
 
 
-def train_lgb(X_train, y_train, X_val, y_val, date):
+def train_lgb(X_train, y_train, X_val, y_val, features, date):
     # define random hyperparammeters
     params = {'boosting_type': 'gbdt',
               'metric': 'rmse',
@@ -152,8 +282,8 @@ def train_lgb(X_train, y_train, X_val, y_val, date):
               }
 
     # datasetの作成
-    train_set = lgb.Dataset(X_train, y_train)
-    val_set = lgb.Dataset(X_val, y_val)
+    train_set = lgb.Dataset(X_train[features], y_train)
+    val_set = lgb.Dataset(X_val[features], y_val)
 
     # train/validation
     print("\n[START] training model ->")
@@ -167,9 +297,11 @@ def train_lgb(X_train, y_train, X_val, y_val, date):
     with open(model_path, mode='wb') as fp:
         pickle.dump(model, fp)
 
-    val_pred = model.predict(X_val)
+    val_pred = model.predict(X_val[features])
     val_RMSE_score = np.sqrt(metrics.mean_squared_error(val_pred, y_val))
     print(f'val RMSE score: {val_RMSE_score}')
+    # val_WRMSSE_score = evaluate_WRMSSE()
+    # print(f'val WRMSSE score: {val_WRMSSE_score}')
 
     return model
 
@@ -181,14 +313,14 @@ def predict_and_submmision(model, features, date):
     te = create_data(is_train=False, num_train_day=56)
     cols = [f"F{i+1}" for i in range(28)]
 
-    for tdelta in tqdm(range(0, 28)):
+    for tdelta in tqdm(range(28)):
         day = fday + timedelta(days=tdelta)
         tst = te[(te["date"] >= day - timedelta(days=56)) & (te["date"] <= day)].copy()
         tst = feature_engineering(tst)
-        tst = tst.loc[tst.date == day , features]
+        tst = tst.loc[tst["date"] == day, features]
         te.loc[te.date == day, "demand"] = alpha * model.predict(tst) # magic multiplier by kyakovlev
-
-    stage1_sub = te.loc[te["date"] >= fday, ["id", "demand"]].copy()
+    
+    stage1_sub = te.loc[te["d"] > "d_1913", ["id", "demand"]].copy()
     stage1_sub["F"] = [f"F{rank}" for rank in stage1_sub.groupby("id")["id"].cumcount()+1]
     stage1_sub = stage1_sub.set_index(["id", "F" ]).unstack()["demand"][cols].reset_index()
     stage1_sub.fillna(0., inplace = True)
@@ -208,17 +340,17 @@ def predict_and_submmision(model, features, date):
 
 def main():
     # 変更パラメータ
-    pretrained_model = "../model/20200411/model_235756.pickle"
+    # pretrained_model = "../model/20200411/model_235756.pickle"
 
     t1 = time.time()
     date = datetime.today().strftime("%Y%m%d_%H%M%S")
 
-    # data_df = create_data(is_train=True, num_train_day=1500)
-    # data_df = feature_engineering(data_df)
+    data_df = create_data(is_train=True, num_train_day=1500)
+    data_df = feature_engineering(data_df)
 
     # define list of features
     default_features = ['item_id', 'dept_id', 'store_id','cat_id', 'state_id', 'event_name_1', 'event_type_1',
-                        'event_name_2', 'event_type_1', 'snap_CA', 'snap_TX', 'snap_WI', 'sell_price']
+                        'event_name_2', 'event_type_2', 'snap_CA', 'snap_TX', 'snap_WI', 'sell_price']
     demand_features = ['lag7', 'lag28', 'rmean_lag7_7', 'rmean_lag7_28', 'rmean_lag28_7', 'rmean_lag28_28']
     # price_features = ['sell_price_lag1', 'sell_price_lag7', 'sell_price_lag28', "diff_sell_price", "div_sell_price"]
     time_features = ["year", "month", "week", "quarter", "mday", "wday"]  # , "black_friday"]
@@ -226,12 +358,12 @@ def main():
     print("N_features: {}\n".format(len(features)))
 
     # train/val split
-    # X_train, y_train, X_val, y_val = train_val_split(data_df, features)
+    X_train, y_train, X_val, y_val = train_val_split(data_df)
     
     # train
-    # model = train_lgb(X_train, y_train, X_val, y_val, date)
-    with open(pretrained_model, mode='rb') as fp:
-        model = pickle.load(fp)
+    model = train_lgb(X_train, y_train, X_val, y_val, features, date)
+    # with open(pretrained_model, mode='rb') as fp:
+    #     model = pickle.load(fp)
 
     # predict & submission
     predict_and_submmision(model, features, date)
