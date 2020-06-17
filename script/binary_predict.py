6651a16ab65ebf1805a9c6910f901df9232633a2
kyohei <h27.kubb@gmail.com>
2020-06-17 18:53:25 +0900

    ２値分類を追加

---
+++ script/binary_predict.py
@@ -0,0 +1,216 @@
+import gc
+import json
+import os
+import pickle
+import random
+import time
+from typing import Union
+from IPython.core.display import display
+import traceback
+import psutil
+import warnings
+from datetime import datetime, timedelta
+from typing import Union
+from multiprocessing import Pool 
+
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import seaborn as sns
+from sklearn import metrics, preprocessing
+from tqdm import tqdm
+from evaluator import WRMSSEEvaluator
+from utils import seed_everything, send_slack_notification, send_slack_error_notification
+
+warnings.filterwarnings('ignore')
+
+## Multiprocess Runs
+def df_parallelize_run(func, t_split):
+    N_CORES = psutil.cpu_count()     # Available CPU cores
+    num_cores = np.min([N_CORES,len(t_split)])
+    pool = Pool(num_cores)
+    df = pd.concat(pool.map(func, t_split), axis=1)
+    pool.close()
+    pool.join()
+    return df
+
+
+# Read data every stores
+def get_features(TARGET):
+    BASE     = '../input/m5-simple-fe/grid_part_1.pkl'
+    PRICE    = '../input/m5-simple-fe/grid_part_2.pkl'
+    CALENDAR = '../input/m5-simple-fe/grid_part_3.pkl'
+    LAGS     = '../input/m5-lags-features/lags_df_28.pkl'
+    MEAN_ENC = '../input/m5-custom-features/mean_encoding_df.pkl'
+
+    # FEATURES to remove
+    # These features lead to overfit or values not present in test set
+    remove_features = ['id','state_id', KEY_COLUMN, 'date','wm_yr_wk', 'd', TARGET]
+    mean_features  = ['enc_cat_id_mean','enc_cat_id_std',
+                    'enc_dept_id_mean','enc_dept_id_std',
+                    'enc_item_id_mean','enc_item_id_std'] 
+
+    # Read and contact basic feature
+    df = pd.concat([pd.read_pickle(BASE),
+                    pd.read_pickle(PRICE).iloc[:,2:],
+                    pd.read_pickle(CALENDAR).iloc[:,2:]],
+                    axis=1)
+
+    # Leave only relevant store
+    df = df[df['store_id']=="CA_1"]
+
+    # With memory limits we have to read 
+    # lags and mean encoding features
+    # separately and drop items that we don't need.
+    # As our Features Grids are aligned 
+    # we can use index to keep only necessary rows
+    # Alignment is good for us as concat uses less memory than merge.
+    df2 = pd.read_pickle(MEAN_ENC)[mean_features]
+    df2 = df2[df2.index.isin(df.index)]
+    
+    df3 = pd.read_pickle(LAGS).iloc[:,3:]
+    df3 = df3[df3.index.isin(df.index)]
+    
+    df = pd.concat([df, df2], axis=1)
+    del df2 # to not reach memory limit 
+    
+    df = pd.concat([df, df3], axis=1)
+    del df3 # to not reach memory limit 
+    
+    # Create features list
+    features = [col for col in list(df) if col not in remove_features]
+    del df
+
+    return features
+
+
+# Recombine Test set after training
+def get_base_test(KEY_COLUMN, KEY_IDS, OUTPUT):
+    base_test = pd.DataFrame()
+
+    for key_id in KEY_IDS:
+        temp_df = pd.read_pickle(OUTPUT + 'test_'+key_id+'.pkl')
+        temp_df[KEY_COLUMN] = key_id  # .astype("category")
+        base_test = pd.concat([base_test, temp_df]).reset_index(drop=True)
+    return base_test
+
+
+def make_lag_roll(LAG_DAY):
+    TARGET = "sales"
+    shift_day = LAG_DAY[0]
+    roll_wind = LAG_DAY[1]
+    lag_df = base_test[['id','d',TARGET]]
+    col_name = 'rolling_mean_tmp_'+str(shift_day)+'_'+str(roll_wind)
+    lag_df[col_name] = lag_df.groupby(['id'])[TARGET].transform(lambda x: x.shift(shift_day).rolling(roll_wind).mean())
+    return lag_df[[col_name]]
+
+
+def submission(all_preds, ORIGINAL, OUTPUT, VER):
+    """
+    Reading competition sample submission and
+    merging our predictions
+    As we have predictions only for "_validation" data
+    we need to do fillna() for "_evaluation" items
+    """
+    submission = pd.read_csv(ORIGINAL+'sample_submission.csv')[['id']]
+    submission = submission.merge(all_preds, on=['id'], how='left').fillna(0)
+    submission.to_csv(OUTPUT + 'submission_v'+str(VER) + '.csv', index=False)
+
+
+try:
+    t1 = time.time()
+
+    # var
+    VER = 0                          # Our model version
+    TARGET = "sales"
+    KEY_COLUMN = "store_id"
+    SEED = 5046                      # We want all things
+    seed_everything(SEED)            # to be as deterministic 
+    END_TRAIN   = 1913               # TODO  最後は1941に変更 End day of our train set
+    P_HORIZON   = 28                 # Prediction horizon
+
+    #PATHS for Features
+    ORIGINAL = '../input/m5-forecasting-accuracy/'
+    TRAINED_DIR = "0612_0040/"
+    MODEL = "../model/" + TRAINED_DIR
+    OUTPUT = '../output/' + TRAINED_DIR
+
+    os.makedirs(OUTPUT, exist_ok=True)
+    os.makedirs(MODEL, exist_ok=True)
+
+    # SPLITS for lags creation
+    # SHIFT_DAY  = 28
+    # N_LAGS     = 15
+    # LAGS_SPLIT = [col for col in range(SHIFT_DAY, SHIFT_DAY + N_LAGS)]
+    ROLS_SPLIT = []
+    for i in [1,7,14]:
+        for j in [7,14,30,60]:
+            ROLS_SPLIT.append([i,j])
+
+
+    features = get_features(TARGET)
+
+    # Create Dummy DataFrame to store predictions
+    all_preds = pd.DataFrame()
+
+    # Join back the Test dataset with 
+    # a small part of the training data 
+    # to make recursive features
+    KEY_IDS = list(pd.read_pickle('../input/m5-simple-fe/grid_part_1.pkl')[KEY_COLUMN].unique())
+    # KEY_IDS = list(pd.read_csv(ORIGINAL + 'sales_train_evaluation.csv')[KEY_COLUMN].unique())
+    base_test = get_base_test(KEY_COLUMN, KEY_IDS, OUTPUT)
+
+    # Timer to measure predictions time 
+    main_time = time.time()
+
+    # Loop over each prediction day
+    # As rolling lags are the most timeconsuming
+    # we will calculate it for whole day
+    for PREDICT_DAY in range(1,29):    
+        print('Predict | Day:', PREDICT_DAY)
+        send_slack_notification('Predict | Day:{}'.format(PREDICT_DAY))
+        start_time = time.time()
+
+        # Make temporary grid to calculate rolling lags
+        grid_df = base_test.copy()
+        grid_df = pd.concat([grid_df, df_parallelize_run(make_lag_roll, ROLS_SPLIT)], axis=1)
+        
+        for key_id in KEY_IDS:
+            
+            # Read all our models and make predictions
+            # for each day/store pairs
+            model_name = 'lgb_model_'+key_id+'_v'+str(VER)+'.bin' 
+            model = pickle.load(open(MODEL + model_name, 'rb'))
+            
+            day_mask = base_test['d']==(END_TRAIN + PREDICT_DAY)
+            key_mask = base_test[KEY_COLUMN] == key_id
+            
+            mask = (day_mask)&(key_mask)
+            base_test[TARGET][mask] = model.predict(grid_df[mask][features])
+        
+        # Make good column naming and add to all_preds DataFrame
+        temp_df = base_test[day_mask][['id',TARGET]]
+        temp_df.columns = ['id','F'+str(PREDICT_DAY)]
+        if 'id' in list(all_preds):
+            all_preds = all_preds.merge(temp_df, on=['id'], how='left')
+        else:
+            all_preds = temp_df.copy()
+            
+        print('#'*10, ' %0.2f min round |' % ((time.time() - start_time) / 60),
+                      ' %0.2f min total |' % ((time.time() - main_time) / 60),
+                      ' %0.2f day sales |' % (temp_df['F'+str(PREDICT_DAY)].sum()))
+        del grid_df, temp_df
+        
+    all_preds = all_preds.reset_index(drop=True)
+    all_preds["id"] = all_preds["id"].str.replace("evaluation", "validation") # TODO 後で削除
+    submission(all_preds, ORIGINAL, OUTPUT, VER)
+
+    t2 = time.time()
+    send_slack_notification("FINISH")
+    send_slack_notification("spend time: {}[min]".format(str((t2 - t1) / 60)))
+
+except:
+    send_slack_error_notification("[ERROR]\n" + traceback.format_exc())
+    print(traceback.format_exc())
---
+++ script/binary_train.py
@@ -0,0 +1,178 @@
+import gc
+import json
+import os
+import pickle
+import random
+import time
+from IPython.core.display import display
+import traceback
+import psutil
+import warnings
+from datetime import datetime, timedelta
+from typing import Union
+from multiprocessing import Pool 
+
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import seaborn as sns
+from sklearn import metrics, preprocessing
+from tqdm import tqdm
+from utils import seed_everything, send_slack_notification, send_slack_error_notification
+
+# Read data every stores
+def get_data_by_key_column(KEY_COLUMN, TARGET, START_TRAIN, key_id):
+
+    #PATHS for Features
+    BASE     = '../input/m5-simple-fe/grid_part_1.pkl'
+    PRICE    = '../input/m5-simple-fe/grid_part_2.pkl'
+    CALENDAR = '../input/m5-simple-fe/grid_part_3.pkl'
+    LAGS     = '../input/m5-lags-features/lags_df_28.pkl'
+    MEAN_ENC = '../input/m5-custom-features/mean_encoding_df.pkl'
+    
+
+    # FEATURES to remove
+    # These features lead to overfit or values not present in test set
+    remove_features = ['id','state_id', KEY_COLUMN, 'date','wm_yr_wk','d', TARGET]
+    mean_features  = ['enc_cat_id_mean','enc_cat_id_std',
+                    'enc_dept_id_mean','enc_dept_id_std',
+                    'enc_item_id_mean','enc_item_id_std'] 
+
+    # Read and contact basic feature
+    df = pd.concat([pd.read_pickle(BASE),
+                    pd.read_pickle(PRICE).iloc[:,2:],
+                    pd.read_pickle(CALENDAR).iloc[:,2:]],
+                    axis=1)
+
+    # Leave only relevant store
+    df = df[df[KEY_COLUMN]==key_id]
+
+    # With memory limits we have to read 
+    # lags and mean encoding features
+    # separately and drop items that we don't need.
+    # As our Features Grids are aligned 
+    # we can use index to keep only necessary rows
+    # Alignment is good for us as concat uses less memory than merge.
+    df2 = pd.read_pickle(MEAN_ENC)[mean_features]
+    df2 = df2[df2.index.isin(df.index)]
+    
+    df3 = pd.read_pickle(LAGS).iloc[:,3:]
+    df3 = df3[df3.index.isin(df.index)]
+    
+    df = pd.concat([df, df2], axis=1)
+    del df2 # to not reach memory limit 
+    
+    df = pd.concat([df, df3], axis=1)
+    del df3 # to not reach memory limit 
+    
+    # df["d"] = df["d"].str.strip("d_").astype(int)
+
+    # Create featur es list
+    features = [col for col in list(df) if col not in remove_features]
+    df = df[['id','d',TARGET] + features]
+    
+    # Skipping first n rows
+    df = df[df['d']>=START_TRAIN].reset_index(drop=True)
+    
+    return df, features
+
+
+def main():
+    t1 = time.time()
+
+    # var
+    VER = 0                          # Our model version
+    TARGET = "sales"
+    KEY_COLUMN = 'store_id'     # training each id
+    NUM_CPU = psutil.cpu_count() 
+    SEED = 5046                      # We want all things
+    seed_everything(SEED)            # to be as deterministic 
+
+    NOW_DATE = datetime.today().strftime("%Y%m%d_%H%M%S")
+    ORIGINAL = '../input/m5-forecasting-accuracy/' 
+    OUTPUT = '../output/{}/'.format(NOW_DATE[4:13])
+    MODEL = "../model/{}/".format(NOW_DATE[4:13])  
+    os.makedirs(OUTPUT, exist_ok=True)
+    os.makedirs(MODEL, exist_ok=True)
+
+    #LIMITS and const
+    START_TRAIN = 0                  # We can skip some rows (Nans/faster training)
+    END_TRAIN   = 1913               # TODO 最終的に1941に変更 End day of our train set 
+    P_HORIZON   = 28                 # Prediction horizon
+
+    #key ids list
+    KEY_IDS = list(pd.read_pickle('../input/m5-simple-fe/grid_part_1.pkl')[KEY_COLUMN].unique())
+    print("key id: {}\n".format(KEY_IDS))
+    
+    # Train Models
+    params = {'boosting_type': 'gbdt',
+              'objective': 'binary', 
+              'metric': 'binary_logloss',
+              'subsample': 0.5,
+              'subsample_freq': 1,
+              'seed': SEED,
+              'learning_rate': 0.03,
+              'num_leaves': 2**11-1,
+              'min_data_in_leaf': 2**12-1,
+              'feature_fraction': 0.5,
+              'max_bin': 100,
+              'boost_from_average': False,
+              'verbose': -1,
+              'num_threads':NUM_CPU} 
+
+
+    # training every stores
+    for key_id in KEY_IDS:
+        print('Train', key_id)
+        send_slack_notification("Train:{}".format(key_id))
+        
+        # Get grid for current store
+        grid_df, features = get_data_by_key_column(KEY_COLUMN, TARGET, START_TRAIN, key_id)
+        grid_df.loc[grid_df[TARGET] > 0, TARGET] = 1  # NOTE 0,1のbinary化
+        
+        # mask
+        train_mask = grid_df['d']<=END_TRAIN                                         # 0~1913 最終的には0~1941
+        valid_mask = (END_TRAIN<grid_df['d']) & (grid_df['d']<=END_TRAIN+P_HORIZON)  # 1914~1941  予測対象 最終的には1942-1969
+        preds_mask = grid_df['d']>(END_TRAIN-100)                                    # 1814~1969  再帰的予測のため100日分のbafferを取っている
+        
+        # Apply masks
+        train_data = lgb.Dataset(grid_df[train_mask][features], label=grid_df[train_mask][TARGET])
+        valid_data = lgb.Dataset(grid_df[valid_mask][features], label=grid_df[valid_mask][TARGET])
+        
+        # save lgb dataset as bin to reduce memory
+        train_data.save_binary('train_data.bin')
+        train_data = lgb.Dataset('train_data.bin')
+        
+        # Saving part of the dataset for later predictions
+        # Removing features that we need to calculate recursively = include '_tmp_'
+        grid_df = grid_df[preds_mask].reset_index(drop=True)
+        keep_cols = [col for col in list(grid_df) if '_tmp_' not in col]
+        grid_df = grid_df[keep_cols]
+        grid_df.to_pickle(OUTPUT + 'test_'+key_id+'.pkl')
+        del grid_df
+
+        # train
+        model = lgb.train(params, train_data, valid_sets = [valid_data], num_boost_round=10000, early_stopping_rounds=50, verbose_eval = 100)
+
+        # save the estimator as .bin
+        model_name = 'lgb_model_'+key_id+'_v'+str(VER)+'.bin'  # 保存場所
+        pickle.dump(model, open(MODEL + model_name, 'wb'))
+
+        # Remove temporary files and objects 
+        os.remove('train_data.bin')
+        del train_data, valid_data, model
+        gc.collect()
+
+    t2 = time.time()
+    send_slack_notification("FINISH")
+    send_slack_notification("spend time: {}[min]".format(str((t2 - t1) / 60)))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        send_slack_error_notification("[ERROR]\n" + traceback.format_exc())
+        print(traceback.format_exc())
