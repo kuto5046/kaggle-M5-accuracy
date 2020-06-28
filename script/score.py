b8953f977aef4e38b77b2c1f84c37d67ec6eea69
kyohei <h27.kubb@gmail.com>
2020-06-28 09:01:17 +0900

    stage1, stage2どちらにも対応できるように修正

--- script/score.py
+++ script/score.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from evaluator import WRMSSEEvaluator
 
-valid_preds = pd.read_csv("../sub/submission_v2_2.622.csv")
-valid_preds = valid_preds[valid_preds.id.str.contains("validation")]
+valid_preds = pd.read_csv("../sub/stage2/sub_v1_dept_id_2.853.csv")
+# valid_preds = valid_preds[valid_preds.id.str.contains("validation")]
+valid_preds = valid_preds[valid_preds.id.str.contains("evaluation")]
+
 evaluator = WRMSSEEvaluator()
 print(evaluator.score(valid_preds.iloc[:,1:].to_numpy()))
