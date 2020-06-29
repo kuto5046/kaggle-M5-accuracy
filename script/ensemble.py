c32c01f351264629dba0a4cb7a0d96bedf76e20b
kyohei <h27.kubb@gmail.com>
2020-06-29 23:17:51 +0900

    stage1とstage2両方に対応するように修正

--- script/ensemble.py
+++ script/ensemble.py
@@ -5,21 +5,6 @@ from scipy.optimize import minimize
 from evaluator import WRMSSEEvaluator
 
 
-END_TRAIN = 1913
-
-valid_preds1 = pd.read_csv("../sub/sub_v2_dept_id_0.488.csv")
-valid_preds2 = pd.read_csv("../sub/sub_v2_store_id_0.474.csv")
-valid_preds3 = pd.read_csv("../sub/sub_v2_dept_store_id_0.489.csv")
-# valid_preds4 = pd.read_csv("../sub/sub_v4_dept_id_0.515.csv")
-# valid_preds5 = pd.read_csv("../sub/sub_v4_store_id_0.536.csv")
-# valid_preds6 = pd.read_csv("../sub/sub_v4_dept_store_id_0.569.csv")
-# preds = [valid_preds1, valid_preds2, valid_preds3, valid_preds4, valid_preds5, valid_preds6]
-preds = [valid_preds1, valid_preds2, valid_preds3]
-
-lls = []
-weights = []
-evaluator = WRMSSEEvaluator()
-
 # 与えられた重みでscoreを計算する関数
 def score_func(weights):
     ''' scipy minimize will pass the weights as a numpy array '''
@@ -40,27 +25,42 @@ def submission(pred, score):
     sub.to_csv('../sub/sub_ensemble_' + str(round(score, 3)) + "_" + str(END_TRAIN) + '.csv', index=False)
 
 
-# Optimization runs 100 times.
-for _ in tqdm(range(100)):
-    starting_values = np.random.uniform(size=len(preds))
-    # cons are given as constraints.
-    cons = ({'type':'eq','fun':lambda w: 1-sum(w)})
-    bounds = [(0,1)]*len(preds)
-    
-    res = minimize(score_func, starting_values, constraints=cons,
-                   bounds = bounds, method='SLSQP')
+END_TRAIN = 1941
+valid_preds1 = pd.read_csv("../sub/stage2/sub_v1_dept_id_0.802.csv")
+valid_preds2 = pd.read_csv("../sub/stage2/sub_v1_store_id_0.835.csv")
+valid_preds3 = pd.read_csv("../sub/stage2/sub_v1_dept_store_id_0.772.csv")
+# valid_preds1 = pd.read_csv("../sub/sub_v2_dept_id_0.488.csv")
+# valid_preds2 = pd.read_csv("../sub/sub_v2_store_id_0.474.csv")
+# valid_preds3 = pd.read_csv("../sub/sub_v2_dept_store_id_0.489.csv")
+preds = [valid_preds1, valid_preds2, valid_preds3]
 
-    lls.append(res['fun'])
-    weights.append(res['x'])
+if END_TRAIN == 1913:
 
+    lls = []
+    weights = []
+    evaluator = WRMSSEEvaluator()
 
-bestSC = np.min(lls)
-bestWeight = weights[np.argmin(lls)]
-# bestSC = 0.4735244
-# bestWeight = [8.47557084e-02, 9.15244292e-01, 1.92987987e-17, 0.00000000e+00, 0.00000000e+00, 2.23345648e-17]
+    # Optimization runs 100 times.
+    for _ in tqdm(range(100)):
+        starting_values = np.random.uniform(size=len(preds))
+        # cons are given as constraints.
+        cons = ({'type':'eq','fun':lambda w: 1-sum(w)})
+        bounds = [(0,1)]*len(preds)
+        
+        res = minimize(score_func, starting_values, constraints=cons,
+                    bounds = bounds, method='SLSQP')
 
-print('\n Ensemble Score: {best_score:.7f}'.format(best_score=bestSC))
-print('\n Best Weights: {weights:}'.format(weights=bestWeight))
+        lls.append(res['fun'])
+        weights.append(res['x'])
+    bestSC = np.min(lls)
+    bestWeight = weights[np.argmin(lls)]
+    print('\n Ensemble Score: {best_score:.7f}'.format(best_score=bestSC))
+    print('\n Best Weights: {weights:}'.format(weights=bestWeight))
+
+else:
+    bestSC = 0
+    bestWeight = [0.1, 0.9, 0.0]
+
 
 ensemble_pred = 0
 for weight, pred in zip(bestWeight, preds):
@@ -68,3 +68,17 @@ for weight, pred in zip(bestWeight, preds):
 
 ensemble_pred.insert(0, "id", valid_preds1.id)
 submission(ensemble_pred, bestSC)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
